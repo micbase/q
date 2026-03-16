e53103b6efda3360a2b6905826f4e458634745af
Michael Chang <github@micbase.com>
2026-03-15 22:06:34 -0500

    fix ui dev server

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3200',
